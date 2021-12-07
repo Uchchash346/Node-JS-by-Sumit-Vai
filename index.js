81004c07515244371c441078fc892c545b86b410
Uchchash346 <mru416@gmail.com>
2021-12-07 19:10:36 +0600

    v-5 fS module

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const os = require('os');
-console.log(os.cpus());
+const fs = require('fs');
+// fs.writeFileSync('myfile.txt', 'Hello programmers')
+// fs.appendFileSync('myfile.txt', ' How are you?')
+
+fs.readFile('myfile.txt', (err, data) => {
+    console.log(data.toString());
+});
+console.log('Hello');
