351dd4fd70bb38be5c86b28719ce668aa62f52f1
Uchchash346 <mru416@gmail.com>
2021-12-09 08:43:46 +0600

    v-6 Pipe

--- lesson 6/readStream.js
+++ lesson 6/readStream.js
@@ -1,28 +1,28 @@
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//     if (req.url === '/') {
-//         res.write('<html><head><title>Form</title></head>');
-//         // res.write('<body><form method='post' action='/process'><input name='message' /></form></body>');
-//         res.write('<body><form method="post" action="/process"><input name = "message" /></form></body>')
-//         res.end();
-//     } else if (req.url === '/process' && req.method == "POST") {
-//         const body = [];
-//         req.on('data', (chunk) => {
-//             body.push(chunk)
-//         })
-//         req.on('end', () => {
-//             console.log("Stream finished");
-//             const parseBody = Buffer.concat(body).toString();
-//             console.log(parseBody);
-//         })
-//         res.write('Thank you for submitting.');
-//         res.end();
-//     } else {
-//         res.write('Page not Found');
-//         res.end();
-//     }
-// });
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('<html><head><title>Form</title></head>');
+        // res.write('<body><form method='post' action='/process'><input name='message' /></form></body>');
+        res.write('<body><form method="post" action="/process"><input name = "message" /></form></body>')
+        res.end();
+    } else if (req.url === '/process' && req.method == "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            console.log("Stream finished");
+            const parseBody = Buffer.concat(body).toString();
+            console.log(parseBody);
+        })
+        res.write('Thank you for submitting.');
+        res.end();
+    } else {
+        res.write('Page not Found');
+        res.end();
+    }
+});
 
 
-// server.listen(3000);
-// console.log('Listening on Port 3000');
+server.listen(3000);
+console.log('Listening on Port 3000');
