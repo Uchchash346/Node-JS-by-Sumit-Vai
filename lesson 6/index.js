1145a837c5f47fd99f86bcee37f6d8de93f93840
Uchchash346 <mru416@gmail.com>
2021-12-09 08:31:18 +0600

    v-6 Pipe

--- lesson 6/index.js
+++ lesson 6/index.js
@@ -1,37 +1,9 @@
-// const fs = require('fs');
-// const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
+const fs = require('fs');
+const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
 // ourReadStream.on('data', (chunk) => {
-//     console.log(chunk);
+//     ourWriteStream.write(chunk); 
 // })
-// console.log("Hello");
 
-
-const http = require('http');
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('<html><head><title>Form</title></head>');
-        // res.write('<body><form method='post' action='/process'><input name='message' /></form></body>');
-        res.write('<body><form method="post" action="/process"><input name = "message" /></form></body>')
-        res.end();
-    } else if (req.url === '/process' && req.method == "POST") {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk)
-        })
-        req.on('end', () => {
-            console.log("Stream finished");
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-        })
-        res.write('Thank you for submitting.');
-        res.end();
-    } else {
-        res.write('Page not Found');
-        res.end();
-    }
-});
-
-
-server.listen(3000);
-console.log('Listening on Port 3000');
+ourReadStream.pipe(ourWriteStream);
